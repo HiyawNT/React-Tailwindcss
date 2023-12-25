a57de35af2f966808e809d64121c8b8360815ca0
Hiyaw Nigussie Temitim <hiyawnfourever@gmail.com>
2023-12-25 19:24:19 +0300

    fixed bugs

--- src/components/ImgCards.tsx
+++ src/components/ImgCards.tsx
@@ -23,11 +23,7 @@ const PhotoCard: React.FC<PhotoProps> = ({
           <img className="w-full" src={imageSrc} alt={title} />
           <div className="hover:bg-transparent transition duration-300 absolute bottom-0 top-0 right-0 left-0 bg-gray-900 opacity-25"></div>
         </a>
-        <a href="#!">
-          <div className="absolute bottom-0 left-0 bg-indigo-600 px-4 py-2 text-white text-sm hover:bg-white hover:text-indigo-600 transition duration-500 ease-in-out">
-            Photos
-          </div>
-        </a>
+        <a href="#!"></a>
         <a href="!#">
           <div className="text-sm absolute top-0 right-0 bg-gray-100 px-4  text-gray-500 rounded-md h-7 w-28 flex flex-col items-center justify-center mt-3 mr-3 hover:bg-white hover:text-indigo-600 transition duration-500 ease-in-out">
             <span className="font-bold">{date}</span>
